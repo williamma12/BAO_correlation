2910286e3673caaf621fee61a5d1a4e439ab7927
williamma12 <wma1017@gmail.com>
2017-04-13 17:27:13 -0700

    Added run.sh for post-correlation calculations

---
+++ calculation/run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+COUNTER = 1
+
+forpython3
+
+until [ $COUNT -gt 5 ]; do
+    python cal_cor.py $COUNT
+    python xi2dTOxiLegendre_Cmu_spline.py xi2d/xi2d-$COUNT.txt
+    python plot_corr.py $COUNT
+    let COUNT = COUNT + 1
+done
