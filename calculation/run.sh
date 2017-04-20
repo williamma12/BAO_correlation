12dd9a641d536a8e84e2e69cc4b7e068bc425c62
William Ma <wma1017@gmail.com>
2017-04-20 15:09:35 -0700

    Fixed iterating of COUNTER

--- calculation/run.sh
+++ calculation/run.sh
@@ -8,5 +8,5 @@ until [ $COUNT -gt 5 ]; do
     python cal_cor.py $COUNT
     python xi2dTOxiLegendre_Cmu_spline.py xi2d/xi2d-$COUNT.txt
     python plot_corr.py $COUNT
-    let COUNT = COUNT + 1
+    let COUNT=COUNT+1
 done
