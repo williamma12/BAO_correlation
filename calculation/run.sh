bf5184e779632259e812b50dbba6f5c870a75a0a
williamma12 <wma1017@gmail.com>
2017-04-20 16:01:15 -0700

    Added file counter display

--- calculation/run.sh
+++ calculation/run.sh
@@ -2,11 +2,10 @@
 
 COUNT=1
 
-forpython3
-
 until [ $COUNT -gt 5 ]; do
     python cal_cor.py $COUNT
     python xi2dTOxiLegendre_Cmu_spline.py xi2d/xi2d-$COUNT.txt
     python plot_corr.py $COUNT
+    echo ************FINISHED FILE $COUNT**********
     let COUNT=COUNT+1
 done
