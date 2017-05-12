58e00bc1b6c9b3b26a7c6a522d8a57d6f50dbca0
williamma12 <wma1017@gmail.com>
2017-05-11 22:35:41 -0700

    Uses pandas dataframes and data read in properly

--- Spherical-to-Cartesian.py
+++ Spherical-to-Cartesian.py
@@ -1,11 +1,13 @@
+import pandas as pd
 import numpy as np
 import scipy.integrate as integrate
 import sys
 
 path = '/global/homes/w/wma/correlation/'
 suffix = '.v11.1.release.txt'
-start = int(sys.argv[2])
-end = int(sys.argv[3]) + 1
+start = int(sys.argv[2]) + 4000
+end = int(sys.argv[3]) + 4001
+
 try:
     file = sys.argv[1]
     if file == 'data_north':
@@ -26,11 +28,12 @@ except:
     print("**********************************")
 
 # Cosmological parameters
-OmegaM = 0.274
-OmegaL = 0.726
+OmegaM = 0.24
+OmegaL = 0.7
 OmegaK = 1 - OmegaM - OmegaL
 hubble = 3000./0.7
-w = -1.0
+w = 1.0
+alpha = np.deg2rad(-55.)
 
 # Calculate comoving distance functions
 dc = lambda z : hubble/np.sqrt(OmegaM*(1.+z)**3+OmegaK*(1.+z)**2+OmegaL*(1.+z)**(-3.*(1.+w)))
@@ -40,19 +43,20 @@ for i in range(start, end):
     fname = prefix + str(i) + suffix
     pwd = path + fname
     print(pwd)
-    arr = np.loadtxt(pwd, dtype={'names': ('ra', 'dec', 'red', 'weight_boss', 'weight_cp', 'weight_red', 'veto'), 
-        'formats': ('double', 'double', 'double', 'int', 'int', 'int', 'double')}, usecols=(0, 1, 2, 4, 5, 6, 7))
+    df = pd.read_csv(pwd, comment='#', engine='python', delimiter=' ', usecols=[0, 1, 2, 7], names=['ra', 'dec', 'red', 'veto'])
+    df = df[df.veto == 1]
 
-    np.where(arr['veto']!=1, arr['weight_boss'], 0)
-
-    for j in range(0, len(arr)):
-        w = 1
-        p = integrate.quad(dc, 0, arr[j][2])
-        phi=np.deg2rad(arr[j][0])
-        theta=np.deg2rad(90.-arr[j][1])
+    for index, row in df.iterrows():
+        p = integrate.quad(dc, 0, row['red'])
+        phi=np.deg2rad(row['ra'])
+        theta=np.deg2rad(row['dec'])
         x = p[0] * np.cos(phi) * np.cos(theta)
         y = p[0] * np.sin(phi) * np.cos(theta)
         z = p[0] * np.sin(theta)
         result.append((x,y,z,w))
+        rot_x = x * np.cos(alpha) + z * np.sin(alpha)
+        rot_y = y
+        rot_z = -x * np.sin(alpha) + z * np.cos(alpha)
+        #result.append((rot_x, rot_y, rot_z, w))
         
     np.savetxt(path + str(i) + "_" + str(file) + ".xyzw", result, delimiter='\t')
